09db7b17a859c24779af99b4a9fd607b328896b1
Paradox25 <87411168+Aaliyahhh25@users.noreply.github.com>
2023-02-16 21:58:24 +0800

    homepage js

---
+++ homepage.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+    strings: ["Hello world.", "Hi! I'm Janna Angeles"],
+    typeSpeed: 100,
+    startDelay: 0,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: "|",
+    contentType: 'html'
+});
