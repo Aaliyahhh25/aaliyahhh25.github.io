85f29c250e314be9548c2ffa5ec9151191f4e4a6
Paradox25 <87411168+Aaliyahhh25@users.noreply.github.com>
2023-05-23 21:48:53 +0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,10 +1,15 @@
-$("#typed").typed({
-    strings: ["Hello world.", "Hi! I'm Janna Angeles"],
-    typeSpeed: 100,
-    startDelay: 0,
-    backSpeed: 60,
-    backDelay: 2000,
-    loop: true,
-    cursorChar: "|",
-    contentType: 'html'
+/*message form*/
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+/*home page animation*/
+window.ityped.init(document.querySelector('.ityped'),{
+    strings: ['<Hello World!/>','I am Janna Angeles'],
+        loop: true
 });
+
